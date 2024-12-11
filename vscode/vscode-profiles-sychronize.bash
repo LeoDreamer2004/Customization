56dc80387ea0b26b8851d51b7d9ef8877a18c3fe
LeoDreamer <2200010825@stu.pku.edu.cn>
2024-12-11 16:43:21 +0800

    Updated vscode profile python

--- vscode/vscode-profiles-sychronize.bash
+++ vscode/vscode-profiles-sychronize.bash
@@ -9,22 +9,22 @@ path=$(grep -E "^$key\s*=" $map_path | awk -F '=' '{print $2}' | xargs)
 if [ -n "$path" ]; then
 echo -e "Local \e[1m\e[92mconfig[$key]\e[0m found at $path"
 cp $HOME$path ./settings.json 
-echo -e "\e[1m\e[36mStart copying config to other profiles \e[0m"
+echo -e "\e[1m\e[36mStart copying config to other profiles... \e[0m"
 grep -E "^\s*[^#]\w+\s*=" $map_path | grep -v -E "^\s*$key\s*=" | while IFS="=" read -r other_key other_path; do
 cp ./settings.json $HOME$other_path
 echo -e "Copied \e[1m\e[92mconfig[$key]\e[0m to \e[1m\e[92mconfig[$other_key]\e[0m"
 done
 
-echo -e "Commit changes to git? \e[1m\e[92m(y/n)\e[0m"
+echo -e "Commit changes to git? \e[1m(y/n)\e[0m"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-echo "Committing changes to git"
+echo -e "\e[1m\e[36mCommitting changes to git... \e[0m"
 git add *
 git commit -am "Updated vscode profile $key"
 git push
 fi
 
-echo -e "\e[1m\e[36mAll profiles are synchronized\e[0m"
+echo -e "\e[1m\e[36mAll profiles are synchronized! \e[0m"
 
 else
 echo "Key '$key' not found in $map"
