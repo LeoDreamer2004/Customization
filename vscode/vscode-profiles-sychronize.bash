33230bcc11123095a7ecfe03d77cf5caef72ec01
LeoDreamer <2200010825@stu.pku.edu.cn>
2024-12-11 16:32:09 +0800

    first commit

---
+++ vscode/vscode-profiles-sychronize.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+key="$1"
+map_path="./vscode-profiles-mapping.toml"
+
+path=$(grep -E "^$key\s*=" $map_path | awk -F '=' '{print $2}' | xargs)
+
+if [ -n "$path" ]; then
+echo "Local config[$key] found at $path"
+echo
+cp $HOME$path ./settings.json 
+
+grep -E "^\s*[^#]\w+\s*=" $map_path | grep -v -E "^\s*$key\s*=" | while IFS="=" read -r other_key other_path; do
+cp ./settings.json $HOME$other_path
+echo "Copied config[$key] to config[$other_key]"
+done
+
+git commit -am "Updated vscode profile $key"
+git push
+
+else
+echo "Key '$key' not found in $map"
+fi
